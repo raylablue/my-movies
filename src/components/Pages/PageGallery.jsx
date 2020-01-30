637f9094b1eea44ccad847ba6b975517156255b0
Rachel Blue <rachel17.22@gmail.com>
2020-01-30 11:08:21 -0700

    gallery display now fluid, used bootstrap and scss

--- src/components/Pages/PageGallery.jsx
+++ src/components/Pages/PageGallery.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import './GalleryStyles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import NavBar from '../Organisms/NavBar/NavBar';
 import Footer from '../Organisms/Footer/Footer';
 
@@ -12,6 +15,7 @@ const getMovies = async () => {
   } catch (e) {
     resError = e;
   }
+
   return {
     error: resError,
     data,
@@ -33,12 +37,43 @@ function Gallery() {
   return (
     <>
       <NavBar />
-      <div>
-        {dataError ? dataError.toString() : null}
+      <div className="container-fluid">
+        <div className="row row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-center">
 
-        {movieArray.map((movie) => (
-          <img src={movie.Poster} alt="Movie Poster" />
-        ))}
+          {dataError ? dataError.toString() : null}
+
+          {movieArray.map((movie) => (
+            <div
+              className="my-4 mx-3 p-1 p-gallery__card"
+            >
+              <div className="p-gallery__poster-container">
+                <img
+                  src={movie.Poster}
+                  alt="Movie Poster"
+                  className="p-gallery__poster"
+                />
+              </div>
+              <h4 className="p-gallery__title">
+                {movie.Title}
+              </h4>
+
+              <a
+                className="mx-4 my-1"
+                href="/fav"
+              >
+                <FontAwesomeIcon
+                  className="fa-3x fas fa-heart"
+                  data-fa-transform="shrink-10 up-5"
+                  icon={faHeart}
+                  id="ticket"
+                />
+              </a>
+
+            </div>
+          ))}
+
+
+        </div>
       </div>
 
       <Footer />
