cb0a7fc1ee7ee930fbd7ae262882ae47cd9278ea
Rachel Blue <rachel17.22@gmail.com>
2020-02-23 13:15:05 -0700

    footer and navbar display edits

--- src/components/Organisms/Footer/Footer.jsx
+++ src/components/Organisms/Footer/Footer.jsx
@@ -1,31 +1,13 @@
 import React from 'react';
 import './FooterStyles.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTicketAlt } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
   return (
-    <div className="card mb-3 bg-dark p-4">
-      <div className="row no-gutters">
-
-        <a
-          className="navbar-brand"
-          href="/gallery"
-        >
-          <FontAwesomeIcon
-            className="fas fa-ticket-alt"
-            icon={faTicketAlt}
-          />
-        </a>
-
-        <div className="card-body mx-4">
-          <p className="card-text o-footer__p-text">
-            This is a footer, a place for feets.
-          </p>
-        </div>
-
+    <footer id="sticky-footer" className="py-5 bg-dark text-white-50">
+      <div className="container text-center">
+        <small>Copyright &copy; Your Website</small>
       </div>
-    </div>
+    </footer>
   );
 }
 
--- src/components/Organisms/NavBar/NavBar.jsx
+++ src/components/Organisms/NavBar/NavBar.jsx
@@ -9,37 +9,47 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-sm o-nav-bar__filmstrip mb-4">
 
-      <a className="o-nav-bar__ticket-logo mx-4 my-1" href="/">
-        <FontAwesomeIcon
-          className="fa-4x fas fa-ticket-alt"
-          data-fa-transform="shrink-10 up-5"
-          icon={faTicketAlt}
-          id="ticket"
-        />
-      </a>
+      <div className="row">
 
-      <ul className="navbar-nav mr-auto">
-        <li className="o-nav-bar__nav-item">
-          <NavLink
-            to="/gallery"
-            className="nav-link"
-            activeClassName="chosen"
-          >
-            Gallery
-          </NavLink>
-        </li>
-        <li className="o-nav-bar__nav-item">
-          <NavLink
-            to="/fav"
-            className="nav-link"
-            activeClassName="chosen"
-          >
-            Favourites
-          </NavLink>
-        </li>
-      </ul>
+        <div className="col-2">
+          <a className="o-nav-bar__ticket-logo mx-4 my-1" href="/">
+            <FontAwesomeIcon
+              className="fa-4x fas fa-ticket-alt"
+              data-fa-transform="shrink-10 up-5"
+              icon={faTicketAlt}
+              id="ticket"
+            />
+          </a>
+        </div>
 
-      <SearchBar />
+        <div className="col-lg-4 col-sm-2">
+          <ul className="navbar-nav mr-auto">
+            <li className="o-nav-bar__nav-item">
+              <NavLink
+                to="/gallery"
+                className="nav-link"
+                activeClassName="chosen"
+              >
+                Gallery
+              </NavLink>
+            </li>
+            <li className="o-nav-bar__nav-item">
+              <NavLink
+                to="/fav"
+                className="nav-link"
+                activeClassName="chosen"
+              >
+                Favourites
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-lg-6 offset-md-2 offset-sm-2 offset-lg-0 col-sm-2">
+          <SearchBar />
+        </div>
+
+      </div>
 
     </nav>
   );
