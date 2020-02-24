d1a3266040ef8a5bcfdbf6d01ff931c9049a5256
Rachel Blue <rachel17.22@gmail.com>
2020-02-23 18:10:39 -0700

    Navbar collapse dropdown menu for smaller windows and devices

--- src/components/Organisms/NavBar/NavBar.jsx
+++ src/components/Organisms/NavBar/NavBar.jsx
@@ -1,56 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './NavBarStyles.scss';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTicketAlt } from '@fortawesome/free-solid-svg-icons';
+import { faTicketAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import SearchBar from '../../Atoms/SearchBar';
 
 function NavBar() {
+  const [collapse, setCollapse] = useState();
+  const [toggleClass, setToggleClass] = useState('');
+  const [toggleAria, setToggleAria] = useState('');
+  const [navClass, setNavClass] = useState('');
+
+  const updateToggleClass = (isActive) => {
+    if (isActive) {
+      setToggleClass('navbar-toggler collapsed');
+      setToggleAria('false');
+      setNavClass('navbar-collapse collapse');
+      return;
+    }
+    setToggleClass('navbar-toggler');
+    setToggleAria('true');
+    setNavClass('navbar-collapse collapse show');
+  };
+
+  useEffect(() => {
+    const initialToggleValue = true;
+    setCollapse(initialToggleValue);
+    updateToggleClass(initialToggleValue);
+  }, []);
+
+  const toggleClick = () => {
+    const newToggleValue = !collapse;
+    setCollapse(newToggleValue);
+    updateToggleClass(newToggleValue);
+  };
+
   return (
-    <nav className="navbar navbar-expand-sm o-nav-bar__filmstrip mb-4">
+    <nav className="navbar navbar-expand-lg o-nav-bar__filmstrip mb-4">
+      <a className="o-nav-bar__ticket-logo mx-4 my-1" href="/">
+        <FontAwesomeIcon
+          className="fa-4x fas fa-ticket-alt"
+          data-fa-transform="shrink-10 up-5"
+          icon={faTicketAlt}
+          id="ticket"
+        />
+      </a>
+      <button
+        className={toggleClass}
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded={toggleAria}
+        aria-label="Toggle navigation"
+        onClick={toggleClick}
+      >
+        <FontAwesomeIcon
+          className="fa-2x fas fa-bars"
+          icon={faBars}
+          id="ticket"
+        />
+      </button>
 
-      <div className="row">
-
-        <div className="col-2">
-          <a className="o-nav-bar__ticket-logo mx-4 my-1" href="/">
-            <FontAwesomeIcon
-              className="fa-4x fas fa-ticket-alt"
-              data-fa-transform="shrink-10 up-5"
-              icon={faTicketAlt}
-              id="ticket"
-            />
-          </a>
-        </div>
-
-        <div className="col-lg-4 col-sm-2">
-          <ul className="navbar-nav mr-auto">
-            <li className="o-nav-bar__nav-item">
-              <NavLink
-                to="/gallery"
-                className="nav-link"
-                activeClassName="chosen"
-              >
-                Gallery
-              </NavLink>
-            </li>
-            <li className="o-nav-bar__nav-item">
-              <NavLink
-                to="/fav"
-                className="nav-link"
-                activeClassName="chosen"
-              >
-                Favourites
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <div className="col-lg-6 offset-md-2 offset-sm-2 offset-lg-0 col-sm-2">
-          <SearchBar />
-        </div>
-
+      <div className={navClass} id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="o-nav-bar__nav-item">
+            <NavLink
+              to="/gallery"
+              className="nav-link"
+              activeClassName="chosen"
+            >
+              Gallery
+            </NavLink>
+          </li>
+          <li className="o-nav-bar__nav-item">
+            <NavLink
+              to="/fav"
+              className="nav-link"
+              activeClassName="chosen"
+            >
+              Favourites
+            </NavLink>
+          </li>
+        </ul>
+        <SearchBar />
       </div>
-
     </nav>
   );
 }
