aa0ed9d1e6ef6cdfadada3f9c8eaa2843dd24c91
Rachel Blue <rachel17.22@gmail.com>
2020-02-27 12:22:35 -0700

    default message added to favourites page for when it's empty

--- src/components/Molecules/FavMovies.jsx
+++ src/components/Molecules/FavMovies.jsx
@@ -4,6 +4,18 @@ import MovieResult from '../Atoms/MovieResult';
 const FavMovies = () => {
   const favMovieList = Object.values(localStorage);
 
+  if (localStorage.length === 0) {
+    return (
+      <div>
+        <h1>Favourite Movies</h1>
+        <p>
+            Looks like you don&lsquo;t have any favourite movies yet.
+            Go spread some love and come back here to see all your favourite movies
+            in one place.
+        </p>
+      </div>
+    );
+  }
   return (
     <div className="row">
       {favMovieList.map((favMovie) => (
